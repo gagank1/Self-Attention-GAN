4ddf5650eb2c79dec07bcf1133284dddaf5cfe8d
Gagan Kaushik <gagan@utexas.edu>
2021-12-07 20:26:44 -0600

    fix server.py

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask import Flask, send_file
-import io
-
 app = Flask(__name__)
 
 @app.route('/')
 def image():
-    send_file('image/attn_gf1.png')
+    return send_file('image/attn_gf1.png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
