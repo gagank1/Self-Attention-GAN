061c0c3506b1c78aebcaa8331a6c9a950fc640fb
Gagan Kaushik <gagan@utexas.edu>
2021-12-08 04:41:27 -0600

    webapp works, still needs more content

---
+++ comicgen.py
@@ -0,0 +1,34 @@
+import os
+import torch
+import torch.nn as nn
+from torchvision.utils import save_image
+from sagan_models import Generator
+from utils import *
+
+class ComicGenerator:
+    def __init__(self, model_path):
+        self.weights_path = os.path.join(model_path)
+
+        # Generator params:
+        self.batch_size = 1 # change to 64 if this doesn't work
+        self.imsize = 128
+        self.z_dim = 128
+        self.g_conv_dim = 64
+        
+        self.G = Generator(self.batch_size, self.imsize, self.z_dim, self.g_conv_dim)
+        self.G = nn.DataParallel(self.G)
+        self.G.load_state_dict(torch.load(self.weights_path, map_location='cpu'))
+        self.G.train() # set to training mode
+    
+    def generateImage(self):
+        z = tensor2var(torch.randn(self.batch_size, self.z_dim))
+        fakeim,_,_ = self.G(z)
+        denormed = denorm(fakeim.data)
+
+        try:
+            os.remove('fakeim.png') # delete old generated image
+        except OSError:
+            pass
+
+        save_image(denormed, os.path.join('fakeim.png'))
+
--- server.py
+++ server.py
@@ -1,9 +1,17 @@
-from flask import Flask, send_file
-app = Flask(__name__)
+from flask import Flask, send_file, redirect
+from comicgen import *
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+CG = ComicGenerator('models/final/15650_G.pth')
+
+@app.route('/imgen')
+def image():
+    CG.generateImage()
+    return send_file('fakeim.png')
 
 @app.route('/')
-def image():
-    return send_file('image/attn_gf1.png')
+def home():
+    return redirect('/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
