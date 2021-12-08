e19e0a4ae9a32e29be8481456f55b334616d65f9
Gagan Kaushik <gagan@utexas.edu>
2021-12-08 06:56:53 -0600

    rewrote api to take generator number as argument instead of switching between generators

--- comicgen.py
+++ comicgen.py
@@ -6,7 +6,7 @@ from sagan_models import Generator
 from utils import *
 
 class ComicGenerator:
-    def __init__(self, model_path):
+    def __init__(self, model_path, gen_num):
         self.weights_path = os.path.join(model_path)
 
         # Generator params:
@@ -19,15 +19,7 @@ class ComicGenerator:
         self.G = nn.DataParallel(self.G)
         self.G.load_state_dict(torch.load(self.weights_path, map_location='cpu'))
         self.G.train() # set to training mode
-    
-    def update(self, newpath):
-        self.weights_path = os.path.join(newpath)
-        self.G.load_state_dict(torch.load(self.weights_path, map_location='cpu'))
-        self.G.train() # set to training mode
-        try:
-            os.remove('fakeim.png') # delete old generated image
-        except OSError:
-            pass
+        self.gen_num = gen_num
 
     def generateImage(self):
         z = tensor2var(torch.randn(self.batch_size, self.z_dim))
@@ -35,9 +27,9 @@ class ComicGenerator:
         denormed = denorm(fakeim.data)
 
         try:
-            os.remove('fakeim.png') # delete old generated image
+            os.remove('fakeim'+str(self.gen_num)+'.png') # delete old generated image
         except OSError:
             pass
 
-        save_image(denormed, os.path.join('fakeim.png'))
+        save_image(denormed, os.path.join('fakeim'+str(self.gen_num)+'.png'))
 
--- server.py
+++ server.py
@@ -1,22 +1,38 @@
 from flask import Flask, send_file, redirect, request
 from comicgen import *
+import threading
 
 app = Flask(__name__, static_url_path='', static_folder='static')
-CG = ComicGenerator('models/final/14085_G.pth')
+
 prefix = 'models/final/'
 suffix = '_G.pth'
+CGMap = {
+    '14085': 0,
+    '17215': 1,
+    '21910': 2,
+    '29735': 3
+}
+CGs = [ComicGenerator(prefix+num+suffix, CGMap[num]) for num in CGMap]
+sem = threading.Semaphore()
 
-@app.route('/imgen')
+@app.route('/imgen', methods=['GET'])
 def image():
-    CG.generateImage()
-    return send_file('fakeim.png')
+    arg = request.args['genNum']
+    if arg in CGMap:
+        sem.acquire()
+        CGs[CGMap[arg]].generateImage()
+        resp = send_file('fakeim'+str(CGMap[arg])+'.png')
+        sem.release()
+    else:
+        resp = redirect('/dummy.png')
+    return resp
 
-@app.route('/change', methods=['GET'])
-def changeGen():
-    arg = request.args['newGen']
-    if arg != 'selected':
-        CG.update(prefix+arg+suffix)
-    return "<h1></h1>"
+# @app.route('/change', methods=['GET'])
+# def changeGen():
+#     arg = request.args['newGen']
+#     if arg != 'selected':
+#         CG.update(prefix+arg+suffix)
+#     return "<h1></h1>"
 
 @app.route('/')
 def home():
