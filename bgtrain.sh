680bc250ec1a66308afc4ae5a5079611fae9cdd5
Gagan Kaushik <gagan@utexas.edu>
2021-12-07 18:21:25 -0600

    use pretrained

--- bgtrain.sh
+++ bgtrain.sh
@@ -1,6 +1,3 @@
 # source this file to train
 
-# nohup python train.py --data_path 'data/covers/classes' --save_path 'output2' --parallel &
-
-nohup python main.py --batch_size 64 --imsize 128 --dataset custom --adv_loss hinge --parallel true --total_step 36000 --version sagan_noise &
-
+nohup python main.py --batch_size 64 --imsize 128 --dataset custom --adv_loss hinge --parallel true --total_step 36000 --pretrained_model 4695 --version sagan_noise &
